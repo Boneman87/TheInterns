413a5c956a338969b2b0ed22d4ac3f2b5b11fea4
Oluwasaanu Akinniyi <akinniyijoseph@gmail.com>
2026-04-28 18:56:32 +0100

    Update backend_setup.tf
    Adding unique id to bucket name

--- terraform/backend_setup.tf
+++ terraform/backend_setup.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "terraform_state" {
-  bucket = "theinterns-terraform-state-unique-id" # Reminder: Must be unique!
+  bucket = "theinterns-terraform-state-001" # Reminder: Must be unique!
   lifecycle {
     prevent_destroy = true
   }
