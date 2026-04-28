422c5e8c8398da3553f16ea18c80ea1ee1b88210
Oluwasaanu Akinniyi <akinniyijoseph@gmail.com>
2026-04-29 00:06:27 +0100

    Update runner-setup.sh
    Ansible installation commands and Dynamic Registration commands added.

--- terraform/runner-setup.sh
+++ terraform/runner-setup.sh
@@ -1,26 +1,39 @@
 # This script registers the Control Node. It runs once when the EC2 starts. It installs your DevOps tools and turns the EC2 into a GitHub Runner.
 #!/bin/bash
-# Install Tools: Docker, Git, Terraform, Ansible
+# 1. Update and install basic dependencies
 sudo apt update -y
+sudo apt-get install -y git jq unzip curl software-properties-common
+
+# 2. Install Docker
 sudo apt-get install -y docker.io git libicu74 curl
 sudo systemctl enable --now docker
+sudo usermod -aG docker ubuntu
 
-# Install Terraform
+# 3. Install Ansible
+sudo add-apt-repository --yes --update ppa:ansible/ansible
+sudo apt-get install -y ansible
+
+# 4. Install Terraform
 wget -O- https://hashicorp.com | sudo gpg --dearmor -o /usr/share/keyrings/hashicorp-archive-keyring.gpg
 echo "deb [signed-by=/usr/share/keyrings/hashicorp-archive-keyring.gpg] https://hashicorp.com $(lsb_release -cs) main" | sudo tee /etc/apt/sources.list.d/hashicorp.list
 sudo apt-get update && sudo apt-get install terraform -y
 
-# Download & Setup GitHub Runner
+# 5. Download & Setup GitHub Runner
 mkdir /home/ubuntu/actions-runner && cd /home/ubuntu/actions-runner
 curl -o actions-runner-linux-x64.tar.gz -L https://github.com
 tar xzf ./actions-runner-linux-x64.tar.gz
 chown -R ubuntu:ubuntu /home/ubuntu/actions-runner
 
-# Register the runner using the token passed from Terraform
-# Note: In production, you'd use a dynamic token API call here
-sudo -u ubuntu ./config.sh --url ${repo_url} --token ${gh_token} --name "AWS-Control-Node" --unattended
-sudo -u ec2-user ./config.sh --url ${repo_url} --token ${gh_token} --name "AWS-Control-Node" --unattended
+# 6. Register the runner using Dynamic Token Registration passed from Terraform by GitHub
+REG_TOKEN=$(curl -L \
+  -X POST \
+  -H "Accept: application/vnd.github+json" \
+  -H "Authorization: Bearer ${gh_token}" \
+  -H "X-GitHub-Api-Version: 2022-11-28" \
+  https://github.com \
+  | jq -r .token)
+sudo -u ubuntu ./config.sh --url ${repo_url} --token $${REG_TOKEN} --name "AWS-Control-Node" --unattended
 
-# Install and start as a background service
+# 7. Install and start as a background service
 sudo ./svc.sh install
 sudo ./svc.sh start
