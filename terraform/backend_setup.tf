b5fd6dd48cf300544b9667c6ab168b0d4aca5565
Oluwasaanu Akinniyi <akinniyijoseph@gmail.com>
2026-04-28 18:13:29 +0100

    Create backend_setup.tf

---
+++ terraform/backend_setup.tf
@@ -0,0 +1,16 @@
+resource "aws_s3_bucket" "terraform_state" {
+  bucket = "theinterns-terraform-state-unique-id" # Must be globally unique
+  lifecycle { prevent_destroy = true }
+}
+
+resource "aws_s3_bucket_versioning" "enabled" {
+  bucket = aws_s3_bucket.terraform_state.id
+  versioning_configuration { status = "Enabled" }
+}
+
+resource "aws_dynamodb_table" "terraform_locks" {
+  name         = "terraform-state-locking"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key     = "LockID"
+  attribute { name = "LockID"; type = "S" }
+}
