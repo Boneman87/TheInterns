3e89bb7d6f5948cdc8f3ba0ea9a71e29c4e9b11d
Oluwasaanu Akinniyi <akinniyijoseph@gmail.com>
2026-04-28 23:40:54 +0100

    Update main.tf
    Added tags to network resources, new network resources and mapped public IP to instance

--- terraform/main.tf
+++ terraform/main.tf
@@ -14,15 +14,54 @@ provider "aws" {
 }
 
 # 1. Create a VPC and Public Subnet
-resource "aws_vpc" "theinterns" { cidr_block = "10.0.0.0/16" }
-
-resource "aws_subnet" "public" {
-  vpc_id     = aws_vpc.theinterns.id
-  cidr_block = "10.0.1.0/24"
-  map_public_ip_on_launch = true
+resource "aws_vpc" "theinterns" {
+  cidr_block = "10.0.0.0/16"
+  tags = {
+    Name = "TheInterns-VPC"
+  }
 }
 
-# 2. IAM Role config for SSM (So you can log in without SSH).
+# 2. Create the Internet Gateway (Required for internet access)
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.theinterns.id
+  
+  tags = {
+    Name = "TheInterns-IGW"
+  }
+}
+
+# 3. Create the Public Subnet with Name and Auto-IP
+resource "aws_subnet" "public" {
+  vpc_id                  = aws_vpc.theinterns.id
+  cidr_block              = "10.0.1.0/24"
+  map_public_ip_on_launch = true # Gives instances a Public IP automatically
+  
+  tags = {
+    Name = "TheInterns-Public-Subnet"
+  }
+}
+
+# 4. Create a Route Table (The "Map" to the Internet)
+resource "aws_route_table" "public_rt" {
+  vpc_id = aws_vpc.theinterns.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "TheInterns-Public-RT"
+  }
+}
+
+# 5. Link the Subnet to the Route Table
+resource "aws_route_table_association" "public_assoc" {
+  subnet_id      = aws_subnet.public.id
+  route_table_id = aws_route_table.public_rt.id
+}
+
+# 6. IAM Role config for SSM (So you can log in without SSH).
 # Trust Policy: This defines who can use the role (EC2)
 resource "aws_iam_role" "ssm_role" {
   name = "ControlNodeSSMRole"
@@ -32,23 +71,43 @@ resource "aws_iam_role" "ssm_role" {
   })
 }
 
-# Access Permisisions Policy: This defines what the role can do. (Minimum permissions for an instance to use Systems and Sessions Manager) 
+# 7. Access Permisisions Policy: This defines what the role can do. (Minimum permissions for an instance to use Systems and Sessions Manager) 
 resource "aws_iam_role_policy_attachment" "ssm_attach" {
   role       = aws_iam_role.ssm_role.name
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+# 8. IAM Instance Profile: attaches the role to the instance.
 resource "aws_iam_instance_profile" "ssm_profile" {
   name = "ControlNodeProfile"
   role = aws_iam_role.ssm_role.name
 }
 
-# 3. The Control Node Instance
+# Adding the Control Node Security Group
+resource "aws_security_group" "control_node_sg" {
+  name        = "control-node-sg"
+  vpc_id      = aws_vpc.theinterns.id
+  description = "Security group for DevOps Control Node"
+
+  # No Inbound rules needed for SSM!
+  
+  egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  tags = { Name = "Control-Node-SG" }
+}
+
+# 9. The Control Node Instance
 resource "aws_instance" "control_node" {
   ami           = "ami-0596cf3199908321b" # Amazon Ubuntu 24.04 LTS AMI
   instance_type = "c7i-flex.large"
   subnet_id     = aws_subnet.public.id
   iam_instance_profile = aws_iam_instance_profile.ssm_profile.name
+  vpc_security_group_ids = [aws_security_group.control_node_sg.id]
 
   # Pass the shell script to the instance
   user_data = templatefile("runner-setup.sh", {
