17b6d9fb2c3ea49ea2cb42b59600e1c21c212246
Oluwasaanu Akinniyi <akinniyijoseph@gmail.com>
2026-04-30 16:21:13 +0100

    Create data.tf
    code to fetch ami details

---
+++ terraform/data.tf
@@ -0,0 +1,11 @@
+# 1. THE DATA SOURCE (The "Search Engine")
+# This finds the AMI ID and stores it in memory
+data "aws_ami" "ubuntu_24_04" {
+  most_recent = true
+  owners      = ["099720109477"]
+
+  filter {
+    name   = "name"
+    values = ["ubuntu/images/hvm-ssd-gp3/ubuntu-noble-24.04-amd64-server-*"]
+  }
+}
